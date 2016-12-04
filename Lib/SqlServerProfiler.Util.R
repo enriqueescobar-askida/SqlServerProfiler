7d62f063f73caa9ad95e7fd91822667a717f0320
enriqueescobar-askida <enrique.escobar@askida.com>
2016-12-04 14:18:14 -0500

    xlsx lib

--- Lib/SqlServerProfiler.Util.R
+++ Lib/SqlServerProfiler.Util.R
@@ -41,32 +41,6 @@ FrequencyTableToTibble <- function(aTable = NULL, aTitle = NULL) {
   
   return(aDataFrame)
 }
-library(xlsx);
-#' Title  TraceXmlXlsToTibble
-#'
-#' @param filePath 
-#'
-#' @return tibble
-#' @export TBD
-#'
-#' @examples TBD
-TraceXmlXlsToTibble <- function(filePath = "") {
-
-  if (is.null(filePath)) {
-
-    return(NULL);
-  } else {
-    if (is.list(filePath)) {
-      filePath <- unlist(filePath);
-    }
-    xlsFile <- xlsx::read.xlsx2(filePath,
-                                sheetIndex = 1,
-                                as.data.frame = TRUE,
-                                header = FALSE);
-      
-    return(tibble::as_data_frame(xlsFile[-2]));
-  }
-}
 #' Title  IsConvertibleToDate
 #'
 #' @param anObject 
@@ -296,49 +270,8 @@ WorcloudToBarplot <- function(aSortedDataFrame = NULL){
     return(aBarplot);
   }
 }
-#' Title  XlsToDataFrameSingleRow
-#'
-#' @param xlsFile 
-#'
-#' @return tibble
-#' @export TBD
-#'
-#' @examples TBD
-XlsToDataFrameSingleRow <- function(xlsFile = ""){
-  aDF <- NULL;
-  
-  if (!is.null(xlsFile)) {
-    aDF <- xlsx::read.xlsx2(xlsFile,
-                                sheetIndex = 1,
-                                as.data.frame = TRUE,
-                                header = TRUE);
-    aDF <- head(aDF, 1);
-    aDF <- tibble::as_data_frame(aDF);
-  }
-  
-  return(aDF);
-}
-#' Title  XlsToDataFrame
-#'
-#' @param xlsFile 
-#'
-#' @return tibble
-#' @export TBD
-#'
-#' @examples TBD
-XlsToDataFrame <- function(xlsFile = ""){
-  aDF <- NULL;
-  
-  if (!is.null(xlsFile)) {
-    aDF <- xlsx::read.xlsx2(xlsFile,
-                            sheetIndex = 1,
-                            as.data.frame = TRUE,
-                            header = TRUE);
-    aDF <- tibble::as_data_frame(aDF);
-  }
-  
-  return(aDF);
-}
+### import xlsx
+source("Lib/xlsx.Util.R");
 #' Title  XlsFileToPng
 #'
 #' @param xlsFilePath 
@@ -466,5 +399,5 @@ ConstraintToTableNameFrequency <- function(aDataFrame = NULL) {
   
   return(aDF);
 }
-# ggplot2
+### ggplot2
 source("Lib/ggplot2.Util.R");
---
+++ Lib/xlsx.Util.R
@@ -0,0 +1,73 @@
+### import
+library(xlsx);
+###
+### functions
+###
+#' Title  XlsToDataFrameSingleRow
+#'
+#' @param xlsFile 
+#'
+#' @return tibble
+#' @export TBD
+#'
+#' @examples TBD
+XlsToDataFrameSingleRow <- function(xlsFile = ""){
+  aDF <- NULL;
+  
+  if (!is.null(xlsFile)) {
+    aDF <- xlsx::read.xlsx2(xlsFile,
+                            sheetIndex = 1,
+                            as.data.frame = TRUE,
+                            header = TRUE);
+    aDF <- head(aDF, 1);
+    aDF <- tibble::as_data_frame(aDF);
+  }
+  
+  return(aDF);
+}
+#' Title  XlsToDataFrame
+#'
+#' @param xlsFile 
+#'
+#' @return tibble
+#' @export TBD
+#'
+#' @examples TBD
+XlsToDataFrame <- function(xlsFile = ""){
+  aDF <- NULL;
+  
+  if (!is.null(xlsFile)) {
+    aDF <- xlsx::read.xlsx2(xlsFile,
+                            sheetIndex = 1,
+                            as.data.frame = TRUE,
+                            header = TRUE);
+    aDF <- tibble::as_data_frame(aDF);
+  }
+  
+  return(aDF);
+}
+#' Title  TraceXmlXlsToTibble
+#'
+#' @param filePath 
+#'
+#' @return tibble
+#' @export TBD
+#'
+#' @examples TBD
+TraceXmlXlsToTibble <- function(filePath = "") {
+  
+  if (is.null(filePath)) {
+    
+    return(NULL);
+  } else {
+    if (is.list(filePath)) {
+      filePath <- unlist(filePath);
+    }
+    xlsFile <- xlsx::read.xlsx2(filePath,
+                                sheetIndex = 1,
+                                as.data.frame = TRUE,
+                                header = FALSE);
+    
+    return(tibble::as_data_frame(xlsFile[-2]));
+  }
+}
