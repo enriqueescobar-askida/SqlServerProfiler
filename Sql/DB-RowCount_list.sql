5847f527a4353180370dd56a11d566e234f6e414
enriqueescobar-askida <Enrique.Escobar@genome.ulaval.ca>
2017-06-05 16:58:17 -0400

    row count sql

--- Sql/DB-RowCount_list.sql
+++ Sql/DB-RowCount_list.sql
@@ -1,3 +1,9 @@
+IF OBJECT_ID(N'#counts', N'U') IS NOT NULL DROP TABLE #counts;
+
+CREATE TABLE #counts (TableName VARCHAR(255), TableRows INT);
+
+EXEC sp_MSForEachTable @command1='INSERT #counts (TableName, TableRows) SELECT ''?'', COUNT(*) FROM ?';
+
 SELECT  TableName
         , TableRows
 FROM    #counts
